c2cee196a63c6d2bf4b4536c373dc012f6bff24b
mnoheliard <marthardgzn@gmail.com>
2026-04-10 15:47:27 -0700

    Create JFrame with menu bar and items
    Added a simple JFrame with a menu bar containing 'File' and 'About' menus.

--- Main.java
+++ Main.java
@@ -1,1 +1,23 @@
+import javax.swing.*;
 
+public class Main extends JFrame{
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.setSize(400,300);
+        main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        main.setVisible(true);
+        main.setTitle("Frame");
+    }
+
+    public Main()
+    {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenu aboutMenu = new JMenu("About");
+        JMenuItem openItem = new JMenuItem("Open");
+        JMenuItem saveItem = new JMenuItem("Open");
+        JMenuItem openItem = new JMenuItem("Open");
+
+
+    }
+}
