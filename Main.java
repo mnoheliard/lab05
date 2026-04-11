4cf8eabfcc5fb2951c3479124190e0d37c727569
mnoheliard <marthardgzn@gmail.com>
2026-04-10 17:46:40 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main extends JFrame{
     public static void main(String[] args) {
@@ -11,13 +12,33 @@ public class Main extends JFrame{
 
     public Main()
     {
+        DrawArea drawArea= new DrawArea();
+        ToolBar toolBar = new ToolBar();
+        StatusBar statusBar = new StatusBar();
+        BorderLayout layout = new BorderLayout();
+        this.setLayout(layout);
+        this.add(toolBar, BorderLayout.WEST);
+        this.add(statusBar, BorderLayout.SOUTH);
+        this.add(drawArea, BorderLayout.CENTER);
+
+
         JMenuBar menuBar = new JMenuBar();
         JMenu fileMenu = new JMenu("File");
         JMenu aboutMenu = new JMenu("About");
-        JMenuItem openItem = new JMenuItem("Open");
-        JMenuItem saveItem = new JMenuItem("Open");
-        JMenuItem openItem = new JMenuItem("Open");
+        JMenu helpMenu = new JMenu("Help");
 
+        JMenuItem openItem = new JMenuItem("Open");
+        JMenuItem saveItem = new JMenuItem("Save");
+        JMenuItem exitItem = new JMenuItem("Exit");
+
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(exitItem);
+
+        menuBar.add(fileMenu);
+        menuBar.add(aboutMenu);
+        menuBar.add(helpMenu);
+        setJMenuBar(menuBar);
 
     }
 }
