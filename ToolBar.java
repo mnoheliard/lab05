8e96ea8fb9126fdbbfe3c451d2eba5bae2b77f7a
mnoheliard <marthardgzn@gmail.com>
2026-04-10 17:50:19 -0700

    Update ToolBar.java

--- ToolBar.java
+++ ToolBar.java
@@ -1,1 +1,20 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
+
+class ToolBar extends JPanel implements ActionListener {
+
+    public ToolBar() {
+        this.setBackground(Color.YELLOW);
+
+        JButton button = new JButton("Tool Bar");
+        button.addActionListener(this);
+        this.add(button);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Clicked");
+    }
+}
