dc094ca8fca72fe93836fe1d6d5963a13c8ea224
mnoheliard <marthardgzn@gmail.com>
2026-04-10 17:48:37 -0700

    Add StatusBar class with JLabel and layout

--- StatusBar.java
+++ StatusBar.java
@@ -1,1 +1,12 @@
+import java.awt.*;
+import javax.swing.*;
 
+class StatusBar extends JPanel{
+    private JLabel statusLabel;
+    public StatusBar() {
+        this.setLayout(new FlowLayout(FlowLayout.CENTER));
+        this.setBackground(Color.MAGENTA);
+        statusLabel = new JLabel("status bar");
+        this.add(statusLabel);
+    }
+}
